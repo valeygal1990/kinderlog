37c420e71e1429a2fa8ec66510be53304abab583
Valentine Sakwa <valeygal360@gmail.com>
2016-08-16 21:15:19 -0400

    initial commit

---
+++ JS/kinderlog.js
@@ -0,0 +1,37 @@
+Document.ready(function){
+
+
+
+//User logs in and clicks on start a new log
+//User selects todays date
+//User clicks on next button
+$('.nextBtn').on('click',loadChildsNameForm);
+
+function loadChildsNameForm{
+	event.prevent(default);
+	$('.childsNameForm').show();
+}
+
+
+
+
+}
+//child's Name form loads
+//user enters child's name
+//User clicks on next button
+//Activity form loads
+//user enters activity notes
+//user clicks next button
+//duration form loads
+//user enters activity duration (time calculator?)
+//user clicks complete log
+//entire log story is displayed for review. reads "Sloane went to the park today and played with Reeves for 2 hours etc"
+//save log and email log buttons are displayed
+//if user chooses to save log...save the log
+//if user chooses to email the log...email the log.
+//buttons to start new log or sign out display
+//Page loads 
+
+
+
+}
